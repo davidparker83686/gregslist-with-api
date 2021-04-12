0c13260b12590a532544d8122e058d55ac3a10b8
davidparker83686 <davidjparker91@gmail.com>
2021-04-11 23:16:15 -0600

    done

--- app/app/AppState.js
+++ app/app/AppState.js
@@ -1,10 +1,15 @@
-import Value from "./Models/Value.js"
+import Car from "./Models/Car.js"
+import House from "./Models/House.js"
+import Job from "./Models/Job.js"
+
 import { EventEmitter } from "./Utils/EventEmitter.js"
 import { isValidProp } from "./Utils/isValidProp.js"
 
 class AppState extends EventEmitter {
-  /** @type {Value[]} */
-  values = []
+  /** @type {Car[]} */
+  cars = []
+  jobs = []
+  houses = []
 }
 
 export const ProxyState = new Proxy(new AppState(), {
--- app/main.js
+++
@@ -1,7 +0,0 @@
-import ValuesController from "./Controllers/ValuesController.js";
-
-class App {
-  valuesController = new ValuesController();
-}
-
-window["app"] = new App();
